0dbe39ed464c70c90df2e9d46e2f85efa388089e
YeongKi <dty3152@gmail.com>
2020-06-22 18:17:46 +0900

    Initial commit

---
+++ 005/formatting.js
@@ -0,0 +1,6 @@
+console.log('name:%s','yeongki');
+console.log('age:%d',26);
+console.log('pi:%d',3.14);
+console.log('math:%d science:%d',92,84);
+console.log('name:%s age:%d science:%d', 'yeongki', 26, 84)
+console.log('name:%s math:%s science:%s', 'yeongki', 92, 84)
---
+++ 006/formatting2.js
@@ -0,0 +1,13 @@
+const greeting1='hello';
+const greeting2='bye';
+const name1='yeongki';
+const name2='gaon';
+
+const statement1=`${greeting1}! my name is ${name1}`;
+const statement2=`${greeting2}! my name is ${name2}`;
+
+console.log(`${greeting1}! my name is ${name1}`);
+console.log(`${greeting2}! my name is ${name2}`);
+
+console.log('statement1', statement1);
+console.log('statement2', statement2);
